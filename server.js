697ee641f0207f3f8764930f5c0609a1aeac8f2b
Ryan Acham <ryanacham@hotmail.com>
2022-06-28 18:36:30 -0400

    changed hard setting of port to whatever heroku dynamically sets

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.set('view engine', 'handlebars');
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(process.env.PORT, () => console.log('Now listening'));
 });
